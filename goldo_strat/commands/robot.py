4a9718b9a650bc9a5fc4f2c709dab829cd7d11fb
Goldorak <echelon@free.fr>
2025-05-30 02:13:33 +0200

    * Add direct FGPA register access

--- goldo_strat/commands/robot.py
+++ goldo_strat/commands/robot.py
@@ -12,6 +12,10 @@ import runpy
 class RobotCommands:
     def __init__(self, robot):
         self._robot = robot
+        self._broker = self._robot._broker
+        self._apb_addr = 0
+        self._apb_data = 0
+        self._broker.registerCallback('nucleo/out/fpga/reg', self.onFpgaRegRead)
         
     def loadConfig(self):
         self._gpio_ids = {}
@@ -47,18 +51,31 @@ class RobotCommands:
     def sensors(self):
         return self._robot._state_proto.sensors
         
-    @property
-    def tryOhm(self):
-        k = (self.sensors['tryohm_bit_0'], self.sensors['tryohm_bit_1'])
-        m = {
-            (True, True): None,
-            (True, False): 'yellow',
-            (False, True): 'red',
-            (False, False): 'purple'
-            }
-        return m.get(k)
-        
     async def gpioSet(self, name, value):
         gpio_id = self._gpio_ids[name]
         await self._publish('nucleo/in/gpio/set',
                             _sym_db.GetSymbol('goldo.nucleo.gpio.CmdGpioSet')(sequence_number=0, gpio_id=gpio_id, value=value))
+
+    async def fpgaRegWrite(self, reg_addr, value):
+        #print ("  fpgaRegWrite() : addr={:8x} data={:8x}".format(reg_addr, value))
+        await self._publish('nucleo/in/fpga/reg/write',
+                            _sym_db.GetSymbol('goldo.nucleo.fpga.RegWrite')(apb_address = reg_addr, apb_value = value))
+
+    async def fpgaRegRead(self, reg_addr):
+        self._apb_addr = 0
+        self._apb_data = -1
+        await self._publish('nucleo/in/fpga/reg/read',
+                            _sym_db.GetSymbol('goldo.nucleo.fpga.RegRead')(apb_address = reg_addr))
+        for i in range(0,10):
+            await asyncio.sleep(0.02)
+            if (self._apb_addr == reg_addr):
+                break
+        #print ("  fpgaRegRead() : addr={:8x} data={:8x}".format(self._apb_addr, self._apb_data))
+        return self._apb_data
+
+    async def onFpgaRegRead(self, msg):
+        self._apb_addr = msg.apb_address
+        self._apb_data = msg.apb_value
+        #print ("  onFpgaRegRead() : addr={:8x} data={:8x}".format(self._apb_addr, self._apb_data))
+
+
